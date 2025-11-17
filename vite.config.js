f0466e0fe3d3f77cb02ab8a20a955be42b38f1f9
MargotTerrier <margotterrier04@gmail.com>
2025-11-17 22:45:42 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ListeNoel/',  // <--- IMPORTANT: juste le nom du repo entre '/'
+  base: '/ListeNoel', 
 })
